f1371a53c62958be5162d3c1436adf9443a5458a
Damia <adebitandamola02@gmail.com>
2022-07-02 12:04:04 +0100

    calculator

---
+++ index.js
@@ -0,0 +1,11 @@
+// const number = document.querySelectorAll("#num-value");
+// const input = document.querySelector(".number-input");
+
+
+
+// for (let i =  0; i < number.length; i++) {
+//     number[i].addEventListener("click", function() {
+//         console.log(number[i])
+//         input.value = number[i].textContent        
+//     });
+// }
